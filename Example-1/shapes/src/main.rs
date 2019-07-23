cb00ae3f2903f6623b3042e1945035450b02bb76
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-07-23 16:21:53 -0400

    Rewrite main function to mirror C++ Shapes Example

--- Example-1/shapes/src/main.rs
+++ Example-1/shapes/src/main.rs
@@ -2,13 +2,62 @@ extern crate shapes;
 
 use shapes::shape::Shape;
 use shapes::circle::Circle;
+use shapes::equilateral_triangle::EquilateralTriangle;
 
 use std::fmt;
 
-fn main() {
-    let s = Circle::new();
-    let s2 = Circle::with_radius(2 as f64);
+const PROGRAM_HEADING: [&'static str; 2] = ["Objects & Inheritance: 2-D Shapes",
+                                            "Thomas J. Kennedy"];
 
-    println!("{}", s);
-    println!("{}", s2);
+fn main() {
+    // Print Program Heading
+    println!("{}", "-".repeat(80));
+
+    for &line in PROGRAM_HEADING.iter() {
+        println!("{:^80}", line);
+    }
+
+    println!("{}", "-".repeat(80));
+
+    // Create one RightTriangle
+    //rht_tri = RightTriangle(1, 2)
+
+    //tri = rht_tri  # Point tri to rht_tri
+    // let shape = rht_tri  // Point shape to rht_tri
+
+    // Is this a valid assignment?
+    // let eql_tri = &rht_tri;
+    // Yes, because Python is loosely typed.
+
+    println!("{:^38}", "Display a Right Triangle (rht_tri)");
+    println!("{}", "-".repeat(38));
+    // println!("{}", rht_tri);
+
+    println!("{:^38}", "Display a Right Triangle (tri)");
+    println!("{}", "-".repeat(38));
+    // println!("{}", tri);
+
+    println!("{:^38}", "Display a Right Triangle (shape)");
+    println!("{}", "-".repeat(38));
+    // println!("{}", shape);
+
+    println!("{}", "~".repeat(80));
+
+    // Create one Equilateral Triangle
+    let eql_tri = EquilateralTriangle::with_side(8.0);
+
+    let tri = &eql_tri;  // Point tri to rht_tri
+    let shape = &eql_tri;  // Point shape to rht_tri
+
+    println!("{:^38}", "Display an Eql. Triangle (eql_tri)");
+    println!("{}", "-".repeat(38));
+    println!("{}", eql_tri);
+
+    println!("{:^38}", "Display an Eql. Triangle (tri)");
+    println!("{}", "-".repeat(38));
+    println!("{}", tri);
+
+    println!("{:^38}" ,"Display an Eql. Triangle (shape)");
+    println!("{}", "-".repeat(38));
+    println!("{}", shape);
 }

8ed25937449e061a9f1e351ed1a3f069a731a98a
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-07-24 16:11:35 -0400

    Add missing triangle Display trait. Fix powi vs sqrt typo.

--- Example-1/shapes/src/triangle.rs
+++ Example-1/shapes/src/triangle.rs
@@ -40,7 +40,18 @@ impl Shape for Triangle {
 
         let s = self.perimeter() / 2.0;
 
-        (s * (s - self.side_a) * (s - self.side_b) * (s - self.side_c)).powi(2)
+        (s * (s - self.side_a) * (s - self.side_b) * (s - self.side_c)).sqrt()
+    }
+}
+
+impl fmt::Display for Triangle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "{:12}:{:>24}", "Name", self.name());
+        writeln!(f, "{:12}:{:>24.4}", "Side A", self.side_a);
+        writeln!(f, "{:12}:{:>24.4}", "Side B", self.side_b);
+        writeln!(f, "{:12}:{:>24.4}", "Side C", self.side_c);
+        writeln!(f, "{:12}:{:>24.4}", "Perimeter", self.perimeter());
+        writeln!(f, "{:12}:{:>24.4}", "Area", self.area())
     }
 }
 
@@ -69,6 +80,61 @@ mod tests {
         assert_that!(generic.side_a, close_to(3.0, 0.01));
         assert_that!(generic.side_b, close_to(4.0, 0.01));
         assert_that!(generic.side_c, close_to(5.0, 0.01));
+    }
+
+    #[test]
+    fn test_area() {
+        let generic = Triangle::new();
+        let fancy = Triangle::with_sides(3.0, 4.0, 5.0);
+
+        // Based on 1/2 base * height (side=1)
+        let expected_area: f64 = 3.0_f64.sqrt() / 4.0;
+        assert_that!(generic.area(), close_to(expected_area, 1e-8));
+
+        // Based on 1/2 side * height (side=3)
+        let expected_area: f64 = 0.5 * 3.0 * 4.0;
+        assert_that!(fancy.area(), close_to(expected_area, 1e-8));
+    }
+
+    #[test]
+    fn test_perimeter() {
+        let generic = Triangle::new();
+        let fancy = Triangle::with_sides(3.0, 4.0, 5.0);
+
+        assert_that!(generic.perimeter(), close_to(3.0, 1e-8));
+        assert_that!(fancy.perimeter(), close_to(12.0, 1e-8));
+    }
+
+    #[test]
+    fn test_str() {
+        let fancy = Triangle::with_sides(3.0, 4.0, 5.0);
+        let fancy_str = fancy.to_string();
+
+        assert!(fancy_str.starts_with("Name"));
+        assert!(fancy_str.contains("Triangle"));
+        assert!(fancy_str.ends_with("\n"));
+
+        assert!(fancy_str.contains(&format!("{:12}:{:>24.4}",
+                                            "Perimeter",
+                                            fancy.perimeter())));
+
+        assert!(fancy_str.contains(&format!("{:12}:{:>24.4}",
+                                            "Area",
+                                            fancy.area())));
+
+        assert!(fancy_str.contains(&format!("{:12}:{:>24.4}",
+                                            "Side A",
+                                            fancy.side_a)));
+
+        assert!(fancy_str.contains(&format!("{:12}:{:>24.4}",
+                                            "Side B",
+                                            fancy.side_b)));
+
+        assert!(fancy_str.contains(&format!("{:12}:{:>24.4}",
+                                            "Side C",
+                                            fancy.side_c)));
+
+        assert!(fancy_str.ends_with("\n"));
     }
 
 
